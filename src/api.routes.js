8ab6da46d3e9ce61a00bcd2848fb4814a257a06a
Hector Rodriguez <hector@hrod.dev>
2023-05-08 05:28:30 -0500

    Compare with develop branch

--- src/api.routes.js
+++ src/api.routes.js
@@ -1,13 +1,9 @@
-// Node Modules
 import express from 'express';
 
-// Routes
 import authRoutes from './routes/auth.routes.js';
 import commentRouter from './routes/comment.routes.js';
 import placeId from './routes/placeId.route.js';
 import maps from './routes/maps.routes.js';
-
-
 
 function apiRouter(app) {
   const router = express.Router();
