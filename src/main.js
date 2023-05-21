02aa135c9ee74aee65c17f20ed6d4cad488a741e
Hector Rodriguez <hector@hrod.dev>
2023-05-21 14:48:07 -0500

    Fix: Lint warnings Feature: Add Express Validator

--- src/main.js
+++ src/main.js
@@ -1,33 +1,35 @@
 // Node modules
-import express from 'express';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
-import * as mongodb from './db.js';
-dotenv.config();
+import express from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import validator from 'express-validator'
 
 // Import API
-import apiRouter from './api.routes.js';
+import apiRouter from './api.routes.js'
+
+import * as mongodb from './db.js'
+dotenv.config()
 
 // Initialization
-const app = express();
+const app = express()
 
 // Settings
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT || 3000
 
 // Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(cors());
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+app.use(validator())
 
-await mongodb.connectDB();
+await mongodb.connectDB()
 
 // Routes
-apiRouter(app);
+apiRouter(app)
 
 // Start Server
 app.listen(port, () => {
-  console.log(`Server on port ${port}`);
-});
+  console.log(`Server on port ${port}`)
+})
