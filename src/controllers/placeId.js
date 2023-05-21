51a9b1d20cb396f1e3946f691899a39239c2c25b
Hector Rodriguez <hector@hrod.dev>
2023-05-21 07:48:03 -0500

    Feature: Place Details Update respose format and add address components

--- src/controllers/placeId.js
+++ src/controllers/placeId.js
@@ -8,13 +8,24 @@ TODO: Add secure access
 */
 export const getPlaceId = async (req, res, next) => {
   try {
-    // const placeId = "ChIJN1t_tDeuEmsRUsoyG83frY4";
     const placeId = req.params.place_id
     const keyId = process.env.GOOGLE_MAPS_API_KEY
     const { data } = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Crating%2Cformatted_address%2Cphoto%2Cwheelchair_accessible_entrance%2Copening_hours&place_id=${placeId}&key=${keyId}`
+      `https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Crating%2Cformatted_address%2Caddress_components%2Cformatted_phone_number%2Cphoto%2Cwheelchair_accessible_entrance%2Copening_hours&place_id=${placeId}&key=${keyId}`
     )
-    res.status(200).json(data.result)
+    const {formatted_address, name, opening_hours, photos, wheelchair_accessible_entrance, formatted_phone_number, address_components } = data.result
+    const { weekday_text } = opening_hours
+    res.status(200).json({
+      data: {
+        formatted_address,
+        address_components,
+        name,
+        formatted_phone_number,
+        weekday_text,
+        wheelchair_accessible_entrance,
+        photos
+    }
+  })
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'Error' })
