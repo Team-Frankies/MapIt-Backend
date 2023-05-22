4dbd9e6a1e3d9b325a77480b417435d4dbc583c9
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-05-22 12:06:51 -0600

    feat: :sparkles: add contact route

--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -1,18 +1,18 @@
-import { Router } from 'express';
-import { body } from 'express-validator';
-import { create } from '../controllers/contact.controller';
-import { validate } from '../middlewares/validator';
+import { Router } from 'express'
+import { body } from 'express-validator'
+import * as conctacController from '../controllers/contact.controller.js'
+import { validate } from '../middlewares/validator.js'
 
-const router = Router();
+const router = Router()
 
 router.post(
-	'/',
-	validate([
-		body('name').notEmpty().withMessage('Name is required'),
-		body('email').isEmail().withMessage('Email is required'),
-        body('message').notEmpty().withMessage('Message is required')
-	]),
-	create
-);
+  '/',
+  validate([
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Email is required'),
+    body('message').notEmpty().withMessage('Message is required')
+  ]),
+  conctacController.createController
+)
 
-export default router;
+export default router
