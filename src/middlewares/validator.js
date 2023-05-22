ddb80bf10f415e967526c15d495131021ff6d7c7
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-05-22 12:08:02 -0600

    feat: :sparkles: add express-validator middleware

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,16 +1,16 @@
-import { validationResult } from 'express-validator';
+import { validationResult } from 'express-validator'
 
 const validate = (validations) => {
-	return async (req, res, next) => {
-		await Promise.all(validations.map((validation) => validation.run(req)));
+  return async (req, res, next) => {
+    await Promise.all(validations.map((validation) => validation.run(req)))
 
-		const errors = validationResult(req);
-		if (errors.isEmpty()) {
-			return next();
-		}
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+      return next()
+    }
 
-		res.status(400).json({ errors: errors.array() });
-	};
-};
+    res.status(400).json({ errors: errors.array() })
+  }
+}
 
-export { validate };
+export { validate }
