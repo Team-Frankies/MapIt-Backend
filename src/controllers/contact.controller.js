e80a14367ac10284030bfc57f3d5dc3dfdda135a
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-05-22 12:07:39 -0600

    feat: sparkles: add contact resources add contact controller add contact service add contact model

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,3 +1,6 @@
-const create = async (req, res) => {};
-
-export { create };
+import * as contactService from '../services/contact.service.js'
+export async function createController (req, res) {
+  return res
+    .status(201)
+    .json({ message: await contactService.create(req.body) })
+}
--- src/services/contact.service.js
+++ src/services/contact.service.js
@@ -1,3 +1,21 @@
-const createContact = async () => {};
+import Contact from '../models/contact.model.js'
+import { sendMail } from './email.service.js'
 
-export { createContact };
+export async function create (data) {
+  try {
+    const contact = new Contact(data)
+    await contact.save()
+    const emailBody = {
+      subject: 'Contact form',
+      body: `Someone has send a contact form 
+             name: ${data.name} 
+             email: ${data.email} 
+             message: ${data.message}`
+    }
+    await sendMail(emailBody)
+    return 'Record created'
+  } catch (error) {
+    console.log(error)
+    throw new Error('Internal server error')
+  }
+}
