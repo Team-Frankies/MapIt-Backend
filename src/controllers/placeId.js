3011f06589d1a6b08ca1b75678019dfdc1fd08ed
Hector Rodriguez <hector@hrod.dev>
2023-05-23 06:18:34 -0500

    Feature: Place Details Add Spanish language parameters Eslint disable camel case validation from api parameters

--- src/controllers/placeId.js
+++ src/controllers/placeId.js
@@ -11,9 +11,9 @@ export const getPlaceId = async (req, res, next) => {
     const placeId = req.params.place_id
     const keyId = process.env.GOOGLE_MAPS_API_KEY
     const { data } = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Crating%2Cformatted_address%2Caddress_components%2Cformatted_phone_number%2Cphoto%2Cwheelchair_accessible_entrance%2Copening_hours&place_id=${placeId}&key=${keyId}`
+      `https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Crating%2Cformatted_address%2Caddress_components%2Cformatted_phone_number%2Cphoto%2Cwheelchair_accessible_entrance%2Copening_hours&place_id=${placeId}&key=${keyId}&region=ES&language=es`
     )
-    const {formatted_address, name, opening_hours, photos, wheelchair_accessible_entrance, formatted_phone_number, address_components } = data.result
+    const { formatted_address, name, opening_hours, photos, wheelchair_accessible_entrance, formatted_phone_number, address_components } = data.result
     const { weekday_text } = opening_hours
     res.status(200).json({
       data: {
@@ -24,8 +24,8 @@ export const getPlaceId = async (req, res, next) => {
         weekday_text,
         wheelchair_accessible_entrance,
         photos
-    }
-  })
+      }
+    })
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'Error' })
