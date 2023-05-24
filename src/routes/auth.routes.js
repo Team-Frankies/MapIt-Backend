2ff53757d15b17eb661c87fbed207d88e4dee867
Hector Rodriguez <hector@hrod.dev>
2023-05-24 07:17:49 -0500

    Feature: Documentation Add information about the sign up, sign in, logout, place id and user profile

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,12 +5,173 @@ import * as authController from '../controllers/auth.controller.js'
 import * as authMiddleware from '../middlewares/auth.middleware.js'
 const router = Router()
 
+/**
+ * @openapi
+ * '/api/v1/auth/sign-up':
+ *  post:
+ *     tags:
+ *     - Register & Login
+ *     summary: Register a new user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - email
+ *              - firstName
+ *              - lastName
+ *              - password
+ *            properties:
+ *              email:
+ *                type: string
+ *                default: test@gmail.com
+ *              firstname:
+ *                type: string
+ *                default: John
+ *              lastname:
+ *                type: string
+ *                default: Doe
+ *              password:
+ *                type: string
+ *                default: password
+ *     responses:
+ *      201:
+ *        description: Created
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Internal Server Error
+ *
+ */
 router.post('/sign-up', authController.createUser)
+
+/**
+ * @openapi
+ * '/api/v1/auth/sign-in':
+ *  post:
+ *     tags:
+ *     - Register & Login
+ *     summary: Login the user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - email
+ *              - password
+ *            properties:
+ *              email:
+ *                type: string
+ *                default: test@gmail.com
+ *              password:
+ *                type: string
+ *                default: password
+ *     responses:
+ *      200:
+ *        description: Suscess
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Internal Server Error
+ *
+ */
 router.post('/sign-in', authController.signIn)
+
+/**
+ * @openapi
+ * '/api/v1/auth/sign-out':
+ *  post:
+ *     tags:
+ *     - Register & Login
+ *     summary: Logout the user
+ *     requestBody:
+ *      required: false
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *     responses:
+ *      200:
+ *        description: Suscess
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Internal Server Error
+ *
+ */
 router.post('/sign-out', authController.signOut)
-router
-  .get('/user/:id', authMiddleware.verifyToken, authController.userId)
-  .put('/user/:id', authMiddleware.verifyToken, authController.updateUser)
+
+/**
+ * @openapi
+ * '/api/v1/auth/user/:id':
+ *  get:
+ *     tags:
+ *     - User Profile
+ *     summary: Get all heroes
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: ObjectId
+ *                  firstname:
+ *                    type: string
+ *                  lastname:
+ *                    type: string
+ *                  email:
+ *                    type: string
+ *                  comments:
+ *                    type: array
+ *       400:
+ *         description: Bad request
+ */
+router.get('/user/:id', authMiddleware.verifyToken, authController.userId)
+
+/**
+ * @openapi
+ * '/api/v1/auth/user/:id':
+ *  put:
+ *     tags:
+ *     - User Profile
+ *     summary: Modify a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - firstname
+ *              - lastname
+ *              - current password
+ *              - new password
+ *            properties:
+ *              firstname:
+ *                type: string
+ *                default: Juan
+ *              lastname:
+ *                type: string
+ *                default: Perez
+ *              password:
+ *     responses:
+ *      200:
+ *        description: Modified
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ */
+router.put('/user/:id', authMiddleware.verifyToken, authController.updateUser)
 
 router.get('/test-protected', authMiddleware.verifyToken, (req, res) => {
   return res.send('Has access to protected route')
--- src/routes/placeId.route.js
+++ src/routes/placeId.route.js
@@ -3,7 +3,36 @@ import express from 'express'
 import { getPlaceId } from '../controllers/placeId.js'
 
 const router = express.Router()
-
+/**
+ * @openapi
+ * /placeId/{place_id}:
+ *  get:
+ *     tags:
+ *     - Place ID
+ *     description: Returns Google API Places details
+ *     responses:
+ *       200:
+ *         description: Success - Returns places details
+ *          content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: ObjectId
+ *                  firstname:
+ *                    type: string
+ *                  lastname:
+ *                    type: string
+ *                  email:
+ *                    type: string
+ *                  comments:
+ *                    type: array
+ *       400:
+ *         description: Bad request
+ */
 router.route('/:place_id').get(getPlaceId)
 
 export default router
