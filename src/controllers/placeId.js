75c4c24d8815012e3962917eeb2af99f212dc309
Hector Rodriguez <hector@hrod.dev>
2023-05-27 04:19:47 -0500

    Feature: Fix Place Details Remove destructoring of data result and add it validate when the field is undefined

--- src/controllers/placeId.js
+++ src/controllers/placeId.js
@@ -1,31 +1,29 @@
 /* eslint-disable camelcase */
 import axios from 'axios'
-
+import { urlPhotos } from '../services/urlPhotos.js'
 /*
 @desc GET Place detail
 @route /api/v1/places/:place_id
 @access Public
 TODO: Add secure access
 */
+
 export const getPlaceId = async (req, res, next) => {
+  const keyId = process.env.GOOGLE_MAPS_API_KEY
   try {
     const placeId = req.params.place_id
-    const keyId = process.env.GOOGLE_MAPS_API_KEY
     const { data } = await axios.get(
       `https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Crating%2Cformatted_address%2Caddress_components%2Cformatted_phone_number%2Cphoto%2Cwheelchair_accessible_entrance%2Copening_hours&place_id=${placeId}&key=${keyId}&region=ES&language=es`
     )
-    const { formatted_address, name, opening_hours, photos, wheelchair_accessible_entrance, formatted_phone_number, address_components } = data.result
-    const { weekday_text } = opening_hours
-
     res.status(200).json({
       data: {
-        formatAddress: formatted_address,
-        longName: address_components,
-        name,
-        formattedPhoneNumber: formatted_phone_number,
-        weekday: weekday_text,
-        wheelchairAccesibleEntrance: wheelchair_accessible_entrance,
-        photos
+        formatAddress: data.result.formatted_address || null,
+        longName: data.result.address_components || null,
+        name: data.result.name || null,
+        formattedPhoneNumber: data.result.formatted_phone_number || null,
+        weekday: data.result.weekday_text || null,
+        wheelchairAccesibleEntrance: data.result.wheelchair_accessible_entrance || null,
+        photos: await urlPhotos(data.result.photos)
       }
     })
   } catch (err) {
