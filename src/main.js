87f23fbc43e0fa99eeb1711a7217ae03ba45e5c1
Hector Rodriguez <hector@hrod.dev>
2023-05-28 13:44:06 -0500

    Feature modify main file

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import express from 'express'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import https from 'https'
-import fs from 'fs'
 
 // Import API
 import apiRouter from './api.routes.js'
@@ -16,9 +14,6 @@ dotenv.config()
 // Initialization
 const app = express()
 
-// Static files
-app.use(express.static('public'))
-
 // Settings
 const port = process.env.SERVER_PORT || 3000
 
@@ -28,27 +23,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-app.options('*', cors())
-
-// Connect to DB
 await mongodb.connectDB()
 
-// Import certificates
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/vps-3308536-x.dattaweb.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/vps-3308536-x.dattaweb.com/fullchain.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/vps-3308536-x.dattaweb.com/chain.pem')
-}
-
-// Create server
-/* http.createServer(app).listen(80, () => {
-  console.log('Listening...')
-}) */
-
-https.createServer(options, app)
-  .listen(443, () => {
-    console.log(`Server listening on port ${port}`)
-  })
-
 // Routes
+app.use(('/'), express.static('public', { redirect: false }))
+
 apiRouter(app)
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server on port ${port}`)
+})
